522640977ea504811d1f08ab80837aff8a3a6160
Lyssie <lyssiewright.15@gmail.com>
2022-02-03 10:36:24 -0600

    commit tree 387146d3d74dce8205c5a754f370ebac836e4c06

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+console.log("num9: ", customerName);
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'lyssie';
+
+function changeLeastFavoriteCustomer(){
+     leastFavoriteCustomer = 'billy';
+}
